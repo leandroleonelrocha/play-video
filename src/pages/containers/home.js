590660fff2662e30b5ca8e647355acd3a41b3c15
Leandro Leonel Rocha <llrocha@sds_domain_1.local>
2018-06-12 11:30:21 -0300

    first commit

---
+++ src/pages/containers/home.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import HomeLayout from '../components/home-layout';
+import Categories from '../../categories/components/categories';
+import Related from '../components/related';
+import ModalContainer from '../../widgets/containers/modal';
+import Modal from '../../widgets/components/modal';
+class Home extends Component {
+ state = {
+ 	modalVisible:false
+ }
+ handleOpenModal = (event) => {
+ 	this.setState({
+ 		modalVisible:true,
+ 	})
+ }	
+ handleCloseModalClick = (event) => {
+ 		this.setState({
+ 			modalVisible:false,
+ 		})
+ 	}
+
+  render() {
+    return (
+      <HomeLayout>
+        <Related />
+        <Categories categories={this.props.data.categories} handleOpenModal={this.handleOpenModal} />
+        {
+        	this.state.modalVisible &&	
+	        <ModalContainer>
+		        <Modal handleClick = {this.handleCloseModalClick} >
+		        	<h1>Esto es un portal</h1>
+		        </Modal>
+	        </ModalContainer>
+        }
+      </HomeLayout>
+    )
+  }
+}
+
+export default Home
