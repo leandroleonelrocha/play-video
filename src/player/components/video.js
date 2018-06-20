f671a13edfa98c20ddc4ad230de0721515c57b15
leandro rocha <leandrorocha@leandros-MacBook-Pro.local>
2018-06-20 19:38:20 -0300

    update video player

--- src/player/components/video.js
+++ src/player/components/video.js
@@ -3,12 +3,33 @@ import './video.css';
 
 
 class Video extends Component {
+	togglePlay(){
+		if(this.props.pause){
+			this.video.play()
+		}else{
+			this.video.pause()
+		}
+	}
+	componentWillReceiveProps(nextProps){
+		if(nextProps.pause != this.props.pause){
+			this.togglePlay();
+		}
+	}
+	setRef = element =>{
+		this.video = element;
+	}
+	
 	render(){
+		const {
+			handleLoaderMetadata,
+		} = this.props;
 		return (
 			<div className="Video">
 				<video 
 					autoPlay={this.props.autoplay}
 					src={this.props.src}
+					ref={this.setRef}
+					onLoadedMetadata={handleLoaderMetadata}
 				/>
 			</div>	
 
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,17 +2,46 @@ import React,{ Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/Title';
-
+import PlayPause from '../components/play-pause';
+import Timer from '../../player/components/timer';
+import VideoPlayerControls from '../../player/components/video-player-controls';
 class VideoPlayer extends Component{
-
+	state = {
+		pause:true,
+		duration:0,
+	}
+	togglePlay = (event) => {
+		this.setState({
+			pause:!this.state.pause
+		})
+	}
+	componentDidMount(){
+		pause: (!this.state.autoplay)
+	}
+	handleLoaderMetadata = event => {
+		this.video = event.target;
+		this.setState({
+			duration: this.video.duration
+		})
+	}
 	render(){
 		return (
 				<VideoPlayerLayout>
 					<Title
 						title="Sarasa"
 					 />
+					 <VideoPlayerControls>
+					 <PlayPause 
+					 pause={this.state.pause}
+					 handleClick={this.togglePlay} />
+					 <Timer
+					 	duration={this.state.duration} />
+					 </VideoPlayerControls>
+					 
 					<Video 
-						autoplay={false}
+						autoplay={this.props.autoplay}
+						pause={this.state.pause}
+						handleLoaderMetadata={this.handleLoaderMetadata}
 						src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
 					/>
 				</VideoPlayerLayout>
